32f25d3da6becf08f8d91d8630d95aefd052004e
kabya <sb.kabya@gmail.com>
2026-02-28 10:15:18 +0600

    Fix static files for Vercel

--- hotel_booking/wsgi.py
+++ hotel_booking/wsgi.py
@@ -3,14 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_booking.settings')
 
-application = get_wsgi_application()
-
-import django
-from django.conf import settings
-
-staticfiles_dir = os.path.join(settings.BASE_DIR, 'staticfiles')
-if not os.path.exists(staticfiles_dir):
-    from django.core.management import call_command
-    call_command('collectstatic', '--noinput', verbosity=0)
-
-app = application
+app = get_wsgi_application()
+application = app
