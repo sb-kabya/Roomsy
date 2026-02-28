bb493b493b3b943aa26da425b8eb9b84ba5aa941
kabya <sb.kabya@gmail.com>
2026-02-28 10:08:55 +0600

    Fix wsgi collectstatic - run after app ready

--- hotel_booking/wsgi.py
+++ hotel_booking/wsgi.py
@@ -3,10 +3,14 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_booking.settings')
 
-from django.conf import settings
-if not os.path.exists(os.path.join(settings.BASE_DIR, 'staticfiles')):
-    from django.core.management import call_command
-    call_command('collectstatic', '--noinput')
+application = get_wsgi_application()
 
-app = get_wsgi_application()
-application = app
+import django
+from django.conf import settings
+
+staticfiles_dir = os.path.join(settings.BASE_DIR, 'staticfiles')
+if not os.path.exists(staticfiles_dir):
+    from django.core.management import call_command
+    call_command('collectstatic', '--noinput', verbosity=0)
+
+app = application
